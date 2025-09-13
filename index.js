d8980f91eb1e797d18511ffef4bf84d44b4c25dd
loay <161643055+loay-adel@users.noreply.github.com>
2025-09-13 19:35:25 +0300

    fixed the db missname

--- index.js
+++ index.js
@@ -81,9 +81,8 @@ const connectDB = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is not defined in environment variables");
     }
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "ecommerce",
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
